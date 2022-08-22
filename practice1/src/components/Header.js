61424be44f102b0908d1016524aede4485366bab
InfinityPotential <62577792+InfinityPotential@users.noreply.github.com>
2022-08-22 16:20:16 +0700

    upload practice1

---
+++ practice1/src/components/Header.js
@@ -0,0 +1,21 @@
+
+function Header() {
+  return (
+    <div>
+      <nav className="navbar navbar-inverse">
+        <a className="navbar-brand">Component</a>
+        <ul className="nav navbar-nav">
+          <li className="active">
+            <a>Trang chủ</a>
+          </li>
+          <li>
+            <a>Danh mục sản phẩm</a>
+          </li>
+        </ul>
+      </nav>
+      
+    </div>
+  );
+}
+
+export default Header;
---
+++ practice1/src/components/Products.js
@@ -0,0 +1,24 @@
+
+function Product() {
+  return (
+    <div>
+      <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div className="thumbnail">
+          <img alt="laptop" src="https://cdn.tgdd.vn/Products/Images/44/251418/Slider/vi-vn-asus-rog-zephyrus-gaming-g14-ga401qec-r9-k2064t-thumbvideo.jpg"/>
+          <div className="caption">
+            <h3>Laptop Asus ROG Zephyrus G14 Alan Walker GA401QEC R9 5900HS/16GB 1TB/4GB RTX3050Ti/120Hz/Túi/Win10 (K2064T)</h3>
+            <p>
+              37.447.765.95 VND
+            </p>
+            <p>
+              <a className="btn btn-primary">Mua ngay</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Product;
