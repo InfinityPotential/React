a32cc71429d7c43a493fc8b71f754f7ec0bc06ce
InfinityPotential <62577792+InfinityPotential@users.noreply.github.com>
2022-10-04 11:14:48 +0700

    Upload

---
+++ notes/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import {AddPost} from "./AddPost"
+import PostList from "./PostList";
+
+const HomePage = () => {
+    return (
+        <div>
+            <AddPost/>
+            <PostList/>
+        </div>
+    );
+}
+ 
+export default HomePage;
---
+++ notes/src/components/PostList.js
@@ -0,0 +1,19 @@
+import { useSelector } from "react-redux";
+import {PostDetail} from "./PostDetail";
+
+const PostList = () => {
+    const {posts} = useSelector(state =>  state);
+    return (
+        <div className="container">
+            
+            <div className="row">
+                {posts && 
+                posts.map((post)=> <PostDetail post={post} key={post.id}/>)}
+            </div>
+            
+        </div>
+        
+    );
+}
+ 
+export default PostList;
